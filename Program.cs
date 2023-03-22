e3bccd034e1d3f278e47229bd2552e2d3c8f8880
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 01:39:35 +0530

    Employee wage using constructor

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageConstructor
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // EmployeeWageUC1 UC1 = new EmployeeWageUC1();
+            // EmployeeWageUC2 UC2 = new EmployeeWageUC2();
+            EmployeeWageUC3 UC3 = new EmployeeWageUC3();
+        }
+    }
+}
