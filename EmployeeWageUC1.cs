be7fafc2e824a668938ceb5121ee4354f79ce8e2
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 01:40:59 +0530

    Employee wage using constructor

---
+++ EmployeeWageUC1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageConstructor
+{
+    public class EmployeeWageUC1
+
+    {
+        int Is_Present, Is_Not_Present,num;
+        public EmployeeWageUC1()
+
+        {
+            Is_Present = 1;
+            Is_Not_Present = 0;
+            Random random = new Random();
+            int num = random.Next(2);
+
+
+            if(num == 1)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+            
+            
+        }
+    }
+}
