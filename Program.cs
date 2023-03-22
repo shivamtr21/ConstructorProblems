c9710249846e05493460f941498952e343f2cd06
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 02:11:26 +0530

    Employee wage using constructor

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@
     {
         public static void Main(string[] args)
         {
+            //   Console.WriteLine("Welcome To The Emp Wage Problem");
             // EmployeeWageUC1 UC1 = new EmployeeWageUC1();
             // EmployeeWageUC2 UC2 = new EmployeeWageUC2();
-            EmployeeWageUC3 UC3 = new EmployeeWageUC3();
+            //EmployeeWageUC3 UC3 = new EmployeeWageUC3();
+            /*EmployeeWageUC4 UC4 = new EmployeeWageUC4();
+            UC4.Check();
+            EmployeeWageUC5 UC5 = new EmployeeWageUC5();
+            UC5.Check();*/
+            EmployeeWageUC6 UC6 = new EmployeeWageUC6();
+            UC6.Check();
         }
     }
 }
