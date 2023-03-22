d7ae0e19dd8ba22d95705ccc4f984dda05097856
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-23 02:14:03 +0530

    Employee wage using constructor

---
+++ EmployeeWageUC6.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageConstructor
+{
+    public class EmployeeWageUC6
+    {
+        public void Check()
+        {
+            int Max_Days = 20;
+            int Max_Hours = 100;
+
+
+            int Wage_Per_hrs = 20;
+            int EmpHrs = 0;
+
+            int Total_Wage = 0;
+
+
+            int Total_Work_Hour = 0;
+            int day = 1;
+
+
+            while (day <= Max_Days && Total_Work_Hour <= Max_Hours)
+            {
+                Random random = new Random();
+
+                int num = random.Next(3);
+
+                switch (num)
+                {
+                    case 0:
+                        Console.WriteLine("Employee Is Absent");
+                        EmpHrs = 0;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Employee Is Present For Full Time");
+                        EmpHrs = 8;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Employee Is Present For Part Time");
+                        EmpHrs = 4;
+                        break;
+
+                }
+                day++;
+
+                Total_Work_Hour = (Total_Work_Hour + EmpHrs);
+                Total_Wage = (Total_Work_Hour * Wage_Per_hrs);
+
+            }
+
+            Console.WriteLine("Total Work Hour Is " + Total_Work_Hour);
+            Console.WriteLine("Total Salary For A Month Is " + Total_Wage);
+
+
+        }
+    }
+    
+}
